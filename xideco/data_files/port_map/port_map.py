287e147d390927f0df1afb5191ff002e3b18f83b
MisterYsLab@gmail.com <brookwood58>
2016-01-16 13:54:35 -0500

    Initial Release

---
+++ xideco/data_files/port_map/port_map.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Created on January 9 11:39:15 2016
+
+@author: Alan Yorinks
+Copyright (c) 2016 Alan Yorinks All right reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+port_map = {"router_ip_address": "192.168.2.194", "http_port": "50501", "command_publisher_port": "50502", "1": "50503",
+            "2": "50504", "3": "50505", "4": "50506", "5": "50507", "6": "50508", "7": "50509", "8": "505010",
+            "9": "505011", "10": "505012", "reporter_publisher_port": "505013"}
